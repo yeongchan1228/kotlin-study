287a1479bfe3de6ba464ff2de8319614de44d081
최영찬 <gks54410@gmail.com>
2022-08-24 19:32:04 +0900

    Build: Kotlin Mockito Dependency 추가

--- todo-project/build.gradle.kts
+++ todo-project/build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
 
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("com.ninja-squad:springmockk:3.1.1")
 }
 
 tasks.withType<KotlinCompile> {
