f85a0b4fd6d39765f6e69fdaec0d815f12c23df0
최영찬 <gks54410@gmail.com>
2022-08-25 13:44:53 +0900

    Initial commit

---
+++ issue-management-project/src/test/kotlin/kotlinproject/issuemanagementproject/IssueManagementProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package kotlinproject.issuemanagementproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IssueManagementProjectApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
