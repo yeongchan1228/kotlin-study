91f05dc893e52c3b953fe090f9e64034f49fa419
최영찬 <gks54410@gmail.com>
2022-08-25 15:46:44 +0900

    Feat: Issue Dto

---
+++ issue-management-project/issue-service/src/main/kotlin/kotlinproject/issuemanagementproject/issueservice/model/IssueModel.kt
@@ -0,0 +1,49 @@
+package kotlinproject.issuemanagementproject.issueservice.model
+
+import com.fasterxml.jackson.annotation.JsonFormat
+import kotlinproject.issuemanagementproject.issueservice.domain.issue.Issue
+import kotlinproject.issuemanagementproject.issueservice.domain.issue.IssuePriority
+import kotlinproject.issuemanagementproject.issueservice.domain.issue.IssueStatus
+import kotlinproject.issuemanagementproject.issueservice.domain.issue.IssueType
+import java.time.LocalDateTime
+
+data class IssueRequest(
+    val summary: String,
+    val description: String,
+    val type: IssueType,
+    val priority: IssuePriority,
+    val status: IssueStatus,
+)
+
+data class IssueResponse(
+    val id: Long,
+    val summary: String,
+    val description: String,
+    val type: IssueType,
+    val userId: Long,
+    val priority: IssuePriority,
+    val status: IssueStatus,
+
+    @JsonFormat(pattern = "yyyy-MM-dd:HH:mm:ss")
+    val createdDate: LocalDateTime?,
+
+    @JsonFormat(pattern = "yyyy-MM-dd:HH:mm:ss")
+    val lastModifiedDate: LocalDateTime?,
+) {
+    companion object {
+        operator fun invoke(issue: Issue) =
+            with(issue) {
+                IssueResponse (
+                    id = id!!,
+                    summary = summary,
+                    description = description,
+                    type = type,
+                    userId = userId,
+                    priority = priority,
+                    status = status,
+                    createdDate = createdDate,
+                    lastModifiedDate = lastModifiedDate,
+                )
+            }
+    }
+}
