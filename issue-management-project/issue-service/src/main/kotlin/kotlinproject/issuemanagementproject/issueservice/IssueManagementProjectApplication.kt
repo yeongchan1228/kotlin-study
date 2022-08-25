ad648f9f6e6cacf528816b00ed2bcb466ea80d71
최영찬 <gks54410@gmail.com>
2022-08-25 14:48:46 +0900

    Feat: Main Class 생성

---
+++ issue-management-project/issue-service/src/main/kotlin/kotlinproject/issuemanagementproject/issueservice/IssueManagementProjectApplication.kt
@@ -0,0 +1,11 @@
+package kotlinproject.issuemanagementproject.issueservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IssueManagementProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<IssueManagementProjectApplication>(*args)
+}
