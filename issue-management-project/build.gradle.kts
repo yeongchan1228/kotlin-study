7630121eb1dba4ff38c5e2c0ea012d891167c0fe
최영찬 <gks54410@gmail.com>
2022-08-25 14:47:58 +0900

    Build: 멀티 모듈 환경 설정

--- issue-management-project/build.gradle.kts
+++ issue-management-project/build.gradle.kts
@@ -1,39 +1,61 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-plugins {
-    id("org.springframework.boot") version "2.7.3"
+plugins{
+    id("org.springframework.boot") version "2.7.3" apply false
     id("io.spring.dependency-management") version "1.0.13.RELEASE"
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
     kotlin("plugin.jpa") version "1.6.21"
 }
 
-group = "kotlin-project"
-version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility= JavaVersion.VERSION_17
 
-repositories {
-    mavenCentral()
-}
+allprojects{
+    group= "kotlin-project"
+    version= "0.0.1-SNAPSHOT"
 
-dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    runtimeOnly("com.h2database:h2")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "17"
+    repositories{
+        mavenCentral()
     }
 }
 
-tasks.withType<Test> {
-    useJUnitPlatform()
+subprojects{
+    apply(plugin = "kotlin")
+    apply(plugin = "kotlin-spring")
+    apply(plugin = "io.spring.dependency-management")
+
+    dependencies{
+// JWT
+        implementation("com.auth0:java-jwt:3.19.2")
+
+        // Kotlin 로깅
+        implementation("io.github.microutils:kotlin-logging:1.12.5") // kotlin 기반 logging
+
+        // Kotlin
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+        // H2
+        runtimeOnly("com.h2database:h2")
+
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+    }
+
+    dependencyManagement{
+        imports{
+            mavenBom(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES) // 멀티 모듈에서 제대로 된 의존성 가져올 수 있게 해줌
+        }
+    }
+
+    tasks.withType<KotlinCompile>{
+        kotlinOptions{
+            freeCompilerArgs =listOf("-Xjsr305=strict")
+            jvmTarget = "17"
+        }
+    }
+
+    tasks.withType<Test>{
+        useJUnitPlatform()
+    }
 }
--- issue-management-project/settings.gradle.kts
+++ issue-management-project/settings.gradle.kts
@@ -1,1 +1,2 @@
 rootProject.name = "issue-management-project"
+include("issue-service")
