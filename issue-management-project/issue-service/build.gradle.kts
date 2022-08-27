b910455a7f84397117c867cae2b8cb3c664d30d7
최영찬 <gks54410@gmail.com>
2022-08-27 17:17:32 +0900

    Build: allOpen 주석 추가

--- issue-management-project/issue-service/build.gradle.kts
+++ issue-management-project/issue-service/build.gradle.kts
@@ -1,5 +1,13 @@
 apply(plugin = "kotlin-jpa")
 
+//apply(plugin = "kotlin-allopen")
+//
+//allOpen {
+//    annotation("javax.persistence.Entity")
+//    annotation("javax.persistence.Embeddable")
+//    annotation("javax.persistence.MappedSuperclass")
+//}
+
 dependencies{
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
