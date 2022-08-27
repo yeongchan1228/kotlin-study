a1111943b8a770655f4abce5b11cbb7088e45a2a
최영찬 <gks54410@gmail.com>
2022-08-27 19:58:20 +0900

    Feat: ObserverPattern

---
+++ src/main/kotlin/thread/ObserverPattern.kt
@@ -0,0 +1,40 @@
+package thread
+
+import java.util.*
+
+class Coffee(val name: String)
+
+class Barista : Observable() {
+    private lateinit var coffeeName: String
+
+    fun orderCoffee(name: String) {
+        this.coffeeName = name
+    }
+
+    fun makeCoffee() {
+        setChanged() // 변경 여부 업데이트
+        notifyObservers(Coffee(this.coffeeName))
+    }
+}
+
+class Customer(val name: String) : Observer {
+    override fun update(o: Observable?, arg: Any?) {
+        val coffee = arg as Coffee
+        println("${name}이 ${coffee.name}을 받았습니다.")
+    }
+}
+
+fun main() {
+    val barista = Barista()
+    barista.orderCoffee("아이스 아메리카노")
+
+    val customer = Customer("고객1")
+    val customer2 = Customer("고객2")
+    val customer3 = Customer("고객3")
+
+    barista.addObserver(customer)
+    barista.addObserver(customer2)
+    barista.addObserver(customer3)
+
+    barista.makeCoffee()
+}
