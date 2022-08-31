a95045614757216e276521865abec74051390d28
최영찬 <gks54410@gmail.com>
2022-08-31 16:06:50 +0900

    Feat: Jwt -> AuthUser 변환

--- issue-management-project/issue-service/src/main/kotlin/kotlinproject/issuemanagementproject/issueservice/config/WebConfig.kt
+++ issue-management-project/issue-service/src/main/kotlin/kotlinproject/issuemanagementproject/issueservice/config/WebConfig.kt
@@ -1,5 +1,9 @@
 package kotlinproject.issuemanagementproject.issueservice.config
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import kotlinproject.issuemanagementproject.issueservice.exception.UnauthorizedException
+import kotlinx.coroutines.runBlocking
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.MethodParameter
 import org.springframework.stereotype.Component
@@ -7,6 +11,8 @@ import org.springframework.web.bind.support.WebDataBinderFactory
 import org.springframework.web.context.request.NativeWebRequest
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.method.support.ModelAndViewContainer
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.awaitBody
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport
 
 @Configuration
@@ -21,7 +27,9 @@ class WebConfig(
 }
 
 @Component
-class AuthUserHandlerArgumentResolver : HandlerMethodArgumentResolver {
+class AuthUserHandlerArgumentResolver(
+    @Value("\${auth.url}") val authUrl: String,
+) : HandlerMethodArgumentResolver {
 
     override fun supportsParameter(parameter: MethodParameter): Boolean =
         AuthUser::class.java.isAssignableFrom(parameter.parameterType)
@@ -32,15 +40,23 @@ class AuthUserHandlerArgumentResolver : HandlerMethodArgumentResolver {
         webRequest: NativeWebRequest,
         binderFactory: WebDataBinderFactory?
     ): Any? {
-        return AuthUser (
-            userId = 1,
-            username = "test",
-        )
+
+        val authHeader = webRequest.getHeader("Authorization") ?: throw UnauthorizedException()
+
+        return runBlocking {
+            WebClient.create()
+                .get()
+                .uri(authUrl)
+                .header("Authorization", authHeader)
+                .retrieve()
+                .awaitBody<AuthUser>()
+        }
     }
 
 }
 
 data class AuthUser (
+    @JsonProperty("id")
     val userId: Long,
     val username: String,
     val profileUrl: String? = null,
