9d9a40f985cbdb6d05581589fc6531df6dfbcb71
최영찬 <gks54410@gmail.com>
2022-08-31 15:19:48 +0900

    Feat: 타인 username 조회

--- issue-management-project/user-service/src/main/kotlin/kotlinproject/issuemanagementproject/controller/UserController.kt
+++ issue-management-project/user-service/src/main/kotlin/kotlinproject/issuemanagementproject/controller/UserController.kt
@@ -9,6 +9,7 @@ import kotlinproject.issuemanagementproject.service.UserService
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -43,4 +44,8 @@ class UserController(
     ) : MeResponse =
         MeResponse(userService.getByToken(token))
 
+    @GetMapping("/{userId}/username")
+    suspend fun getUsername(@PathVariable("userId") userId: Long): Map<String, String> {
+        return mapOf("reporter" to userService.get(userId).username)
+    }
 }
